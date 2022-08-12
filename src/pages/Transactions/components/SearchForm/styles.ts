27bcb587caad218f6882234370b2d74e13f30bcf
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-08-12 05:47:50 -0300

    feat: create component to search transactions

---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const SearchFormContainer = styled.form`
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    border-radius: 6px;
+    border: none;
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    padding: 1rem;
+
+    &::placeholder {
+      color: ${(props) => props.theme["gray-500"]};
+    }
+  }
+
+  button {
+    display: flex;
+    gap: 0.75rem;
+    color: ${(props) => props.theme["green-300"]};
+    border: 1px solid ${(props) => props.theme["green-300"]};
+    background-color: transparent;
+    padding: 1rem;
+    border-radius: 6px;
+    font-weight: bold;
+    transition: 0.3s all;
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${(props) => props.theme["green-500"]};
+      border-color: ${(props) => props.theme["green-500"]};
+      color: ${(props) => props.theme.white};
+    }
+  }
+`;
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import {
   PriceHightLight,
   TransactionsContainer,
@@ -12,6 +13,7 @@ export function Transactions() {
       <Header />
       <Summary />
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
